bca08fe8004b9415ab8994e7106610e3b36bfc41
duvan narvaez <47201336+duvan177@users.noreply.github.com>
2020-08-07 01:04:55 -0500

    Fix

--- assets/apis.js
+++ assets/apis.js
@@ -1,5 +1,5 @@
 export default {
-    baseURL:"http://174.138.40.79:5000/",
+    baseURL:"https://cors-anywhere.herokuapp.com/http://174.138.40.79:5000/",
     URL: {
     URL_COLOMBIA : "https://sigma-studios.s3-us-west-2.amazonaws.com/test/colombia.json",
     URL_CONTACT : "api/contact",
